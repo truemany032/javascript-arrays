e0e3ca8c908dc8870267029fcaa2be4a357290e6
Suriya <true.many.032@gmail.com>
2025-02-25 14:23:33 +0700

    Assignment 2

--- ex-7.js
+++ ex-7.js
@@ -42,7 +42,15 @@ let orders = [
 ];
 
 // Start coding here
-let isArray;
-let creditCardTypeOfBlindermann;
-let productQuantityOfJoannet;
-let totalPurchaseOfDary;
+let isArray = Array.isArray(orders);
+let creditCardTypeOfBlindermann = orders[2].creditCardType;
+let productQuantityOfJoannet = orders[3].productQuantity;
+let totalPurchaseOfDary = orders[1].productPrice * orders[1].productQuantity;
+
+orders[2].creditCardType = "visa";
+orders.splice(0, 1);
+
+// console.log(orders);
+// console.log(creditCardTypeOfBlindermann);
+// console.log(productQuantityOfJoannet);
+// console.log(totalPurchaseOfDary);
